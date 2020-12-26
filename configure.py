bf7d0457686c46ae51fca7a48ce53165c47e7a0e
andrewkatson <katsonandrew3.5.com>
2020-12-26 10:53:22 -0500

    SUDO_USER is used when on an EC2 to get workspace path

--- configure.py
+++ configure.py
@@ -36,7 +36,13 @@ library_info = [LibraryInfo("libnorm-dev", "libnorm"), LibraryInfo("libunbound-d
                 LibraryInfo("libudev-dev", "libudev")]
 
 # NEED TO FILL THIS IN WITH YOUR PATH TO DENARII FOR THIS TO WORK SORRY
-workspace_path = os.environ['HOME'] + "/denarii"
+workspace_path = "home/andrew/denarii"
+
+# A workspace path that works regardless on EC2s
+possible_workspace_path = "home/" + os.environ["SUDO_USER"] + "/denarii"
+
+if os.path.exists(possible_workspace_path): 
+    workspace_path = possible_workspace_path
 
 
 def download_url(url, save_path, chunk_size=128):
