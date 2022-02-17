82ff68e41ce3ce2033766f97cd8be27392a0a760
Andrew Katson <katsonandrew3.5@gmail.com>
2022-02-16 20:04:38 -0500

    Trying to Get Randomx Building

---
+++ examples/include_this.cpp
@@ -0,0 +1,10 @@
+//
+// Created by katso on 2/16/2022.
+//
+
+#include "include_this.h"
+
+
+void include_this::do_something() {
+  std::string str;
+}
---
+++ examples/include_this.h
@@ -0,0 +1,16 @@
+//
+// Created by katso on 2/16/2022.
+//
+
+#ifndef DENARII_INCLUDE_THIS_H
+#define DENARII_INCLUDE_THIS_H
+
+#include <string>
+
+extern "C" {
+class include_this {
+public:
+    static void do_something();
+};
+}
+#endif //DENARII_INCLUDE_THIS_H
