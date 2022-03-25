f5ea943d34e571ed8897d8167cebe2b33b15d44e
andrewkatson <katsonandrew3.5.com>
2022-03-25 09:40:05 -0400

    try to fix supercop

--- configure.py
+++ configure.py
@@ -356,31 +356,8 @@ def liblmdb(external_dir_path):
 def build_dependencies():
     external_dir_path = workspace_path / "external"
     chdir(external_dir_path)
-
-    miniupnp(external_dir_path)
-
-    chdir(external_dir_path)
-    randomx(external_dir_path)
-
-    chdir(external_dir_path)
     supercop(external_dir_path)
 
-    chdir(external_dir_path)
-    unbound(external_dir_path)
-
-    chdir(external_dir_path)
-    openssl(external_dir_path)
-
-    chdir(external_dir_path)
-    libzmq(external_dir_path)
-
-    chdir(external_dir_path)
-    zlib(external_dir_path)
-
-    chdir(external_dir_path)
-
-    liblmdb(external_dir_path)
-    chdir(external_dir_path)
 
 
 def build_dependencies_win():
@@ -795,14 +772,8 @@ def generate_files_win():
 workspace_path = workspace_path_finder.find_workspace_path()
 print(workspace_path)
 if sys.platform == "linux":
-    print("Importing dependencies \n\n\n\n\n")
-    import_dependencies()
-
     print("Building dependencies \n\n\n\n\n")
     build_dependencies()
-
-    print("Generating files \n\n\n\n\n")
-    generate_files()
 elif sys.platform == "msys":
 
     print("Building dependencies Windows \n\n\n\n\n")
